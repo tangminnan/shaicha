4736a3b3a18ca6c5afae21e8a6e00c25939384c3
zouying <349829327@qq.com>
2019-12-08 22:59:50 +0800

    `

---
+++ shaicha-manager/src/main/java/com/shaicha/information/domain/BuLiangShili.java
@@ -0,0 +1,63 @@
+package com.shaicha.information.domain;
+
+public class BuLiangShili {
+//	private I0}	${I1}	${I2}	${I3}	${I4}	${I5}	${I6}	${I7}	${I8}	${I9}	${I10}
+	
+	private Long qdshilibuliang;//轻度视力不良
+	private Long zdshilibuliang;//中度视力不良
+	private Long weishilibuliang;//重度视力不良
+	private Long buliangtotal;//不良总计
+	private Double qingduzhanbi;//轻度不良占比
+	private Double zhongduzhanbil;//中度不良占比
+	private Double weightzhanbi;//重度占比
+	private Double zongjizhanbi;//不良总计占比
+	public Long getQdshilibuliang() {
+		return qdshilibuliang;
+	}
+	public void setQdshilibuliang(Long qdshilibuliang) {
+		this.qdshilibuliang = qdshilibuliang;
+	}
+	public Long getZdshilibuliang() {
+		return zdshilibuliang;
+	}
+	public void setZdshilibuliang(Long zdshilibuliang) {
+		this.zdshilibuliang = zdshilibuliang;
+	}
+	public Long getWeishilibuliang() {
+		return weishilibuliang;
+	}
+	public void setWeishilibuliang(Long weishilibuliang) {
+		this.weishilibuliang = weishilibuliang;
+	}
+	public Long getBuliangtotal() {
+		return buliangtotal;
+	}
+	public void setBuliangtotal(Long buliangtotal) {
+		this.buliangtotal = buliangtotal;
+	}
+	public Double getQingduzhanbi() {
+		return qingduzhanbi;
+	}
+	public void setQingduzhanbi(Double qingduzhanbi) {
+		this.qingduzhanbi = qingduzhanbi;
+	}
+	public Double getZhongduzhanbil() {
+		return zhongduzhanbil;
+	}
+	public void setZhongduzhanbil(Double zhongduzhanbil) {
+		this.zhongduzhanbil = zhongduzhanbil;
+	}
+	public Double getWeightzhanbi() {
+		return weightzhanbi;
+	}
+	public void setWeightzhanbi(Double weightzhanbi) {
+		this.weightzhanbi = weightzhanbi;
+	}
+	public Double getZongjizhanbi() {
+		return zongjizhanbi;
+	}
+	public void setZongjizhanbi(Double zongjizhanbi) {
+		this.zongjizhanbi = zongjizhanbi;
+	}
+	
+}
---
+++ shaicha-manager/src/main/java/com/shaicha/information/domain/ShiliJinShi.java
@@ -0,0 +1,217 @@
+package com.shaicha.information.domain;
+
+public class ShiliJinShi {
+	
+	private String schoole;//学校
+	private String xuebu;//学制
+	private Long checkNumbers;//检查人数
+	private Long jslcqianqiNumber;//近视临床前期人数
+	private Double jslcgqianqizhanbi;//近视临床前期占比
+	private Long jxjsNumber;//假性近视人数;
+	private Double jxjszhanbi;//假性近视占比
+	private Long didujinshiNumber;//低度近视人数
+	private Double didujinshizhanbi;//低度近视占比;
+	private Long zhongdujinshiNumber;//中度近视人数
+	private Double zhongdujinshizhanbi;//中度近视占比
+	private Long gaodujinshiNumber;//高度近视人数
+	private Double gaodujinshizhanbi;//高度近视占比;
+	private Long jinshizongjiNumber;//近视总人数
+	private Double jinshizongzhanbi;//近视总人数占比
+	private String a;	
+	private String b;	
+	private String c;
+	private String d;	
+	private String e;	
+	private String f;
+	private String g;	
+	private String h;	
+	private String i;
+	private String j;	
+	private String k;
+	private String l;	
+	private String m;
+	private String n;	
+	private String o;
+	public String getSchoole() {
+		return schoole;
+	}
+	public void setSchoole(String schoole) {
+		this.schoole = schoole;
+	}
+	public String getXuebu() {
+		return xuebu;
+	}
+	public void setXuebu(String xuebu) {
+		this.xuebu = xuebu;
+	}
+	public Long getCheckNumbers() {
+		return checkNumbers;
+	}
+	public void setCheckNumbers(Long checkNumbers) {
+		this.checkNumbers = checkNumbers;
+	}
+	public Long getJslcqianqiNumber() {
+		return jslcqianqiNumber;
+	}
+	public void setJslcqianqiNumber(Long jslcqianqiNumber) {
+		this.jslcqianqiNumber = jslcqianqiNumber;
+	}
+	public Double getJslcgqianqizhanbi() {
+		return jslcgqianqizhanbi;
+	}
+	public void setJslcgqianqizhanbi(Double jslcgqianqizhanbi) {
+		this.jslcgqianqizhanbi = jslcgqianqizhanbi;
+	}
+	public Long getJxjsNumber() {
+		return jxjsNumber;
+	}
+	public void setJxjsNumber(Long jxjsNumber) {
+		this.jxjsNumber = jxjsNumber;
+	}
+	public Double getJxjszhanbi() {
+		return jxjszhanbi;
+	}
+	public void setJxjszhanbi(Double jxjszhanbi) {
+		this.jxjszhanbi = jxjszhanbi;
+	}
+	public Long getDidujinshiNumber() {
+		return didujinshiNumber;
+	}
+	public void setDidujinshiNumber(Long didujinshiNumber) {
+		this.didujinshiNumber = didujinshiNumber;
+	}
+	public Double getDidujinshizhanbi() {
+		return didujinshizhanbi;
+	}
+	public void setDidujinshizhanbi(Double didujinshizhanbi) {
+		this.didujinshizhanbi = didujinshizhanbi;
+	}
+	public Long getZhongdujinshiNumber() {
+		return zhongdujinshiNumber;
+	}
+	public void setZhongdujinshiNumber(Long zhongdujinshiNumber) {
+		this.zhongdujinshiNumber = zhongdujinshiNumber;
+	}
+	public Double getZhongdujinshizhanbi() {
+		return zhongdujinshizhanbi;
+	}
+	public void setZhongdujinshizhanbi(Double zhongdujinshizhanbi) {
+		this.zhongdujinshizhanbi = zhongdujinshizhanbi;
+	}
+	public Long getGaodujinshiNumber() {
+		return gaodujinshiNumber;
+	}
+	public void setGaodujinshiNumber(Long gaodujinshiNumber) {
+		this.gaodujinshiNumber = gaodujinshiNumber;
+	}
+	public Double getGaodujinshizhanbi() {
+		return gaodujinshizhanbi;
+	}
+	public void setGaodujinshizhanbi(Double gaodujinshizhanbi) {
+		this.gaodujinshizhanbi = gaodujinshizhanbi;
+	}
+	public Long getJinshizongjiNumber() {
+		return jinshizongjiNumber;
+	}
+	public void setJinshizongjiNumber(Long jinshizongjiNumber) {
+		this.jinshizongjiNumber = jinshizongjiNumber;
+	}
+	public Double getJinshizongzhanbi() {
+		return jinshizongzhanbi;
+	}
+	public void setJinshizongzhanbi(Double jinshizongzhanbi) {
+		this.jinshizongzhanbi = jinshizongzhanbi;
+	}
+	public String getA() {
+		return a;
+	}
+	public void setA(String a) {
+		this.a = a;
+	}
+	public String getB() {
+		return b;
+	}
+	public void setB(String b) {
+		this.b = b;
+	}
+	public String getC() {
+		return c;
+	}
+	public void setC(String c) {
+		this.c = c;
+	}
+	public String getD() {
+		return d;
+	}
+	public void setD(String d) {
+		this.d = d;
+	}
+	public String getE() {
+		return e;
+	}
+	public void setE(String e) {
+		this.e = e;
+	}
+	public String getF() {
+		return f;
+	}
+	public void setF(String f) {
+		this.f = f;
+	}
+	public String getG() {
+		return g;
+	}
+	public void setG(String g) {
+		this.g = g;
+	}
+	public String getH() {
+		return h;
+	}
+	public void setH(String h) {
+		this.h = h;
+	}
+	public String getI() {
+		return i;
+	}
+	public void setI(String i) {
+		this.i = i;
+	}
+	public String getJ() {
+		return j;
+	}
+	public void setJ(String j) {
+		this.j = j;
+	}
+	public String getK() {
+		return k;
+	}
+	public void setK(String k) {
+		this.k = k;
+	}
+	public String getL() {
+		return l;
+	}
+	public void setL(String l) {
+		this.l = l;
+	}
+	public String getM() {
+		return m;
+	}
+	public void setM(String m) {
+		this.m = m;
+	}
+	public String getN() {
+		return n;
+	}
+	public void setN(String n) {
+		this.n = n;
+	}
+	public String getO() {
+		return o;
+	}
+	public void setO(String o) {
+		this.o = o;
+	}
+	
+	
+}
