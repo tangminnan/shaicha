cbbd150c049ed4c1e8aaa7f2a15dc1f6f1fbfa38
kuadlly <1836232324@qq.com>
2020-11-03 10:13:34 +0800

    ~

--- shaicha-manager/src/main/java/com/shaicha/system/config/ExcelUtils.java
+++ shaicha-manager/src/main/java/com/shaicha/system/config/ExcelUtils.java
@@ -69,52 +69,51 @@ public class ExcelUtils {
 		}
 		return workbook;
 	}
-	
+
 	/**
 	 * 根据Cell类型设置数据
-	 * 
+	 *
 	 * @param cell
 	 * @return
 	 */
 	public static String getCellFormatValue(Cell cell) {
 		String cellvalue = "";
-		DecimalFormat df=new DecimalFormat("0");
+		DecimalFormat df=new DecimalFormat("0.00");
 		if (cell != null) {
 			// 判断当前Cell的Type
 			switch (cell.getCellType()) {
-			// 如果当前Cell的Type为NUMERIC
-		   case Cell.CELL_TYPE_NUMERIC:
-				cellvalue = String.valueOf(cell.getNumericCellValue());
-				
-		//		cellvalue = df.format(cell.getNumericCellValue());
-				break;
-			case Cell.CELL_TYPE_FORMULA: {
-				// 判断当前的cell是否为Date
-				if (HSSFDateUtil.isCellDateFormatted(cell)) {
-					Date date = cell.getDateCellValue();
-					cellvalue = DateUtil.dateToString(date);
+				// 如果当前Cell的Type为NUMERIC
+				case Cell.CELL_TYPE_NUMERIC:
+					cellvalue = String.valueOf(cell.getNumericCellValue());
+
+					//cellvalue = df.format(cell.getNumericCellValue());
+					break;
+				case Cell.CELL_TYPE_FORMULA: {
+					// 判断当前的cell是否为Date
+					if (HSSFDateUtil.isCellDateFormatted(cell)) {
+						Date date = cell.getDateCellValue();
+						cellvalue = DateUtil.dateToString(date);
+					}
+					// 如果是纯数字
+					else {
+						// 取得当前Cell的数值
+						cellvalue = df.format(cell.getNumericCellValue());
+					}
+					break;
 				}
-				// 如果是纯数字
-				else {
-					// 取得当前Cell的数值
-				//	cellvalue = df.format(cell.getNumericCellValue());
-					cellvalue =String.valueOf(cell.getNumericCellValue());
-				}
-				break;
-			}
-			case HSSFCell.CELL_TYPE_BOOLEAN:
-				cellvalue = String.valueOf(cell.getBooleanCellValue());
-				break;
-			case HSSFCell.CELL_TYPE_BLANK:
-				cellvalue = "";
-				break;
-			case HSSFCell.CELL_TYPE_STRING:
-				// 取得当前的Cell字符串
-				cellvalue = cell.getRichStringCellValue().getString();
-				break;
-			// 默认的Cell值
-			default:
-				cellvalue = " ";
+				case HSSFCell.CELL_TYPE_BOOLEAN:
+					cellvalue = String.valueOf(cell.getBooleanCellValue());
+					break;
+				case HSSFCell.CELL_TYPE_BLANK:
+					cellvalue = "";
+					break;
+				case HSSFCell.CELL_TYPE_STRING:
+					// 取得当前的Cell字符串
+					cellvalue = cell.getRichStringCellValue().getString();
+					break;
+				// 默认的Cell值
+				default:
+					cellvalue = " ";
 			}
 		} else {
 			cellvalue = "";
